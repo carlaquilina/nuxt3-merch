405d04852b14ca029cd1541f8b5e19a26f8ad7ff
carl <p03004aqc@gmail.com>
2023-04-03 15:48:31 +0200

    Update default.vue
    removed useless link

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,9 +7,6 @@
           <li><NuxtLink to="/">Home</NuxtLink></li>
           <li><NuxtLink to="/about">About</NuxtLink></li>
           <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
-          <li>
-            <NuxtLink to="/products/abc" class="btn">Products abc</NuxtLink>
-          </li>
         </ul>
       </nav>
     </header>
